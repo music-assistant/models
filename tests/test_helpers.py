6855e1177843fc28863ffadcb9ccdf3fea96bf1b
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-10-30 09:55:47 +0100

    Update test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,7 +4,39 @@ import pytest
 
 from music_assistant_models import media_items
 from music_assistant_models.errors import MusicAssistantError
-from music_assistant_models.helpers import uri
+from music_assistant_models.helpers import uri, util
+
+
+def test_version_extract() -> None:
+    """Test the extraction of version from title."""
+    test_str = "Bam Bam (feat. Ed Sheeran)"
+    title, version = util.parse_title_and_version(test_str)
+    assert title == "Bam Bam"
+    assert version == ""
+    test_str = "Bam Bam (feat. Ed Sheeran) - Karaoke Version"
+    title, version = util.parse_title_and_version(test_str)
+    assert title == "Bam Bam"
+    assert version == "Karaoke Version"
+    test_str = "Bam Bam (feat. Ed Sheeran) [Karaoke Version]"
+    title, version = util.parse_title_and_version(test_str)
+    assert title == "Bam Bam"
+    assert version == "Karaoke Version"
+    test_str = "SuperSong (2011 Remaster)"
+    title, version = util.parse_title_and_version(test_str)
+    assert title == "SuperSong"
+    assert version == "2011 Remaster"
+    test_str = "SuperSong (Live at Wembley)"
+    title, version = util.parse_title_and_version(test_str)
+    assert title == "SuperSong"
+    assert version == "Live at Wembley"
+    test_str = "SuperSong (Instrumental)"
+    title, version = util.parse_title_and_version(test_str)
+    assert title == "SuperSong"
+    assert version == "Instrumental"
+    test_str = "SuperSong (Explicit)"
+    title, version = util.parse_title_and_version(test_str)
+    assert title == "SuperSong"
+    assert version == ""
 
 
 async def test_uri_parsing() -> None:
