a3322df0296eb17c7a76d0ec0db25dc1287e70bd
Marvin Schenkel <marvinschenkel@gmail.com>
2025-10-31 14:45:18 +0100

    Reapply "Change message_id type to str"
    This reverts commit 059a16492874f61fc713d75666de3d68378453f9.

--- music_assistant_models/api.py
+++ music_assistant_models/api.py
@@ -15,7 +15,7 @@ from .helpers import get_serializable_value
 class CommandMessage(DataClassORJSONMixin):
     """Model for a Message holding a command from server to client or client to server."""
 
-    message_id: str | int
+    message_id: str
     command: str
     args: dict[str, Any] | None = None
 
