86bec0eebd3da9f40f638c05f8424db4cc502d22
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-01-31 14:51:48 +0100

    Update music_assistant_models/player.py
    Co-authored-by: Marcel van der Veldt <m.vanderveldt@outlook.com>

--- music_assistant_models/player.py
+++ music_assistant_models/player.py
@@ -193,7 +193,7 @@ class PlayerOption(DataClassDictMixin):
     name: str
     type: PlayerOptionType
 
-    # translation_key: optional translation key for this entry (defaults to settings.{key})
+    # translation_key: optional translation key for this entry (defaults to player_options.{id})
     translation_key: str | None = None
     # translation_params: optional parameters for the translation key
     translation_params: list[str] | None = None
