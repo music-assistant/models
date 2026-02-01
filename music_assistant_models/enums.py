31855761f4252f04825ea3902e7e2e3c6f9b742e
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-02-01 15:12:55 +0100

    add PlayerOptionsUpdated Event

--- music_assistant_models/enums.py
+++ music_assistant_models/enums.py
@@ -452,6 +452,7 @@ class EventType(StrEnum):
     PLAYER_REMOVED = "player_removed"
     PLAYER_CONFIG_UPDATED = "player_config_updated"
     PLAYER_DSP_CONFIG_UPDATED = "player_dsp_config_updated"
+    PLAYER_OPTIONS_UPDATED = "player_options_updated"
     DSP_PRESETS_UPDATED = "dsp_presets_updated"
     QUEUE_ADDED = "queue_added"
     QUEUE_UPDATED = "queue_updated"
