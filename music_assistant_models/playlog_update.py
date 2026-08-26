d4aaae4330b147e34965d398ad519a7554e45937
OzGav <gavnosp@hotmail.com>
2026-08-26 16:31:14 +1000

    Add an event for when an item's played state changes (#381)
    Add a playlog updated event

---
+++ music_assistant_models/playlog_update.py
@@ -0,0 +1,23 @@
+"""
+Model(s) for PlaylogUpdate.
+
+This data is sent with the PLAYLOG_UPDATED event.
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from mashumaro import DataClassDictMixin
+
+from .enums import MediaType
+
+
+@dataclass(frozen=True)
+class PlaylogUpdate(DataClassDictMixin):
+    """Object describing the new playlog state of a media item."""
+
+    uri: str
+    media_type: MediaType
+    fully_played: bool
+    seconds_played: int
